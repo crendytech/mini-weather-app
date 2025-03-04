4c2bc55f9804c22279f89b18c355349f41239fe4
Olusegun Olaniyi <crendytech@gmail.com>
2025-03-04 08:23:59 +0000

    cleanup

--- src/redux/slices/weather-api-slice.ts
+++ src/redux/slices/weather-api-slice.ts
@@ -23,11 +23,14 @@ const initialState: WeatherState = {
   weatherData: {},
 };
 
+const apiKey = import.meta.env.VITE_WEATHER_API_KEY;
+
+
 export const fetchWeather = createAsyncThunk(
   "weather/fetchWeather",
   async (city: string) => {
     const response = await fetch(
-      `https://api.weatherapi.com/v1/forecast.json?key=640e30e15f2c4a64a0c181154250303&q=${city}&days=5&aqi=no&alerts=no`
+      `https://api.weatherapi.com/v1/forecast.json?key=${apiKey}&q=${city}&days=5&aqi=no&alerts=no`
     );
     const data = await response.json();
     if (data.error) {
